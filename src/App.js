7510e07dfc560a157a396b53fd66ac1467b79305
Andrey Bakuta <dra1n86@gmail.com>
2020-11-28 13:49:58 +0200

    With bunch of side effects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useCallback, useEffect } from "react";
+import * as yup from "yup";
+import Form from "react-formal";
+import { Machine, interpret } from "xstate";
 
-function App() {
+const events = {
+  RIDE: "RIDE",
+  RIDE_TAXI: "RIDE_TAXI",
+  RIDE_BUS: "RIDE_BUS",
+  WALK: "WALK"
+}
+
+const schema = yup.object({
+  walking: yup.bool(),
+  riding: yup.bool(),
+  ridingBy: yup.string(),
+});
+
+const getHomeMachine = ({ actions }) => Machine({
+  id: 'getHome',
+  initial: 'walking',
+  states: {
+    walking: {
+      on: {
+        [events.RIDE]: 'riding',
+        [events.RIDE_TAXI]: 'riding.taxi',
+        [events.RIDE_BUS]: 'riding.bus'
+      },
+      entry: ["setWalking"]
+    },
+    riding: {
+      initial: 'bus',
+      on: {
+        [events.WALK]: 'walking'
+      },
+      states: {
+        bus: {
+          on: {
+            [events.RIDE_TAXI]: 'taxi'
+          },
+          entry: ["setRidingBus"]
+        },
+        taxi: {
+          on: {
+            [events.RIDE_BUS]: 'bus'
+          },
+          entry: ["setRidingTaxi"]
+        }
+      }
+    }
+  }
+}, {
+  actions
+});
+
+const App = () => {
+  const [model, setModel] = useState({});
+
+  const setWalking = useCallback(() => setModel({ walking: true, riding: false, ridingBy: "" }), [setModel])
+  const setRidingBus = useCallback(() => setModel({ walking: false, riding: true, ridingBy: "bus" }), [setModel])
+  const setRidingTaxi = useCallback(() => setModel({ walking: false, riding: true, ridingBy: "taxi" }), [setModel])
+
+  const stateRef = useRef(
+    interpret(getHomeMachine({
+      actions: {
+        setWalking,
+        setRidingBus,
+        setRidingTaxi
+      }
+    }))
+  );
+  const state = stateRef.current;
+
+  useEffect(() => state.start(), [state]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Get Home After Party</h1>
+      <p>
+        I'm going by
+      </p>
+      <Form
+        schema={schema}
+        value={model}
+      >
+        <div>
+          <label>
+            <Form.Field name='walking' type="checkbox" onChange={() => state.send(events.WALK)} />
+            Walking
+          </label>
+        </div>
+        <div>
+          <label>
+            <Form.Field name='riding' type="checkbox" onChange={() => state.send(events.RIDE)} />
+            Riding
+          </label>
+          <div>
+            <label>
+              <Form.Field name='ridingBy' type="radio" value="bus" onChange={() => state.send(events.RIDE_BUS)} />
+              a bus
+            </label>
+            <label>
+              <Form.Field name='ridingBy' type="radio" value="taxi" onChange={() => state.send(events.RIDE_TAXI)} />
+              taxi
+            </label>
+          </div>
+        </div>
+      </Form>
     </div>
   );
 }
